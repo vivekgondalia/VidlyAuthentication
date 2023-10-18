83d228e9f212371f9e76ea57b9f207044395fd1d
Vivek Gondalia <hire.vivekgondalia@gmail.com>
2023-10-17 18:05:17 -0700

    Seeding a user and role via Migrations

--- VidlyAuthentication/Controllers/AccountController.cs
+++ VidlyAuthentication/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using VidlyAuthentication.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace VidlyAuthentication.Controllers
 {
@@ -155,6 +156,12 @@ namespace VidlyAuthentication.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    //Temp Code - Seeding Users and Roles
+                    var roleStore = new RoleStore<IdentityRole>(new ApplicationDbContext());
+                    var roleManager = new RoleManager<IdentityRole>(roleStore);
+                    await roleManager.CreateAsync(new IdentityRole("CanManageMovies"));
+                    await UserManager.AddToRoleAsync(user.Id, "CanManageMovies");
+
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
