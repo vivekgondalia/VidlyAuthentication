2ea7395847b851ea091c88d02c147c68d0b4aa18
Vivek Gondalia <hire.vivekgondalia@gmail.com>
2023-10-17 18:46:47 -0700

    Capturing more data with Default Register User Form

--- VidlyAuthentication/Models/ApplicationUser.cs
+++ VidlyAuthentication/Models/ApplicationUser.cs
@@ -9,9 +9,9 @@ namespace VidlyAuthentication.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        //[Required]
-        //[StringLength(255)]
-        //public string DrivingLicense { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string DrivingLicense { get; set; }
 
         //[Required]
         //[StringLength(50)]
